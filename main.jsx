73c13ec43dd553924d8cfaa97b16ea075912b167
Polina <144373555+pollee343@users.noreply.github.com>
2025-04-23 15:57:44 +0300

    init site

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './style.css' // если используется Tailwind или базовые стили
+import App from './src/App.jsx'
+import './src/style.css' // если используется Tailwind или базовые стили
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
